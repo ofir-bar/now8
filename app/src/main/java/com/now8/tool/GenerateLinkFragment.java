c387339fb6a1723e942075d248bb6287d1f7ed05
Ofir Bar <ofirbar5@gmail.com>
2018-11-27 09:45:37 +0200

    added button that share generated link on another app

--- app/src/main/java/com/now8/tool/GenerateLinkFragment.java
+++ app/src/main/java/com/now8/tool/GenerateLinkFragment.java
@@ -1,6 +1,11 @@
 package com.now8.tool;
 
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,8 +14,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 
 public class GenerateLinkFragment extends Fragment {
+    private static final String TAG = "GenerateLinkFragment";
+    private static final String WHATSAPP_PACKAGE = "com.whatsapp";
+
+    //Request Codes
+    private static final int WHATSAPP_REQUEST_CODE = 1;
 
 
     public GenerateLinkFragment() {
@@ -21,18 +34,56 @@ public class GenerateLinkFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_generate_link, container, false);
 
-        Button generateLink = container.findViewById(R.id.linkgen);
+        Button generateLink = fragmentView.findViewById(R.id.linkgen);
         generateLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(),"heyyyy" ,Toast.LENGTH_SHORT).show();
+                shareGeneratedLinkToApp("THIS IS JUST SOME LINK", WHATSAPP_PACKAGE);
             }
         });
 
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generate_link, container, false);
+        return fragmentView;
+    }
+
+
+    private void makeToast(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * method share the generated link to another app
+     * @param generatedLink = the unique generated link which will be shared on external app
+     * @param appPackage = the app which will be started
+     */
+    public void shareGeneratedLinkToApp(String generatedLink,String appPackage){
+
+        Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
+        whatsappIntent.putExtra(Intent.EXTRA_TEXT, generatedLink);
+        whatsappIntent.setPackage(appPackage);
+        whatsappIntent.setType("text/plain");
+        startActivityForResult(whatsappIntent,WHATSAPP_REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK) {
+            if (requestCode == WHATSAPP_REQUEST_CODE) {
+                makeToast(getContext(), "shared successfully");
+            }
+        }
+
+        if (resultCode == RESULT_CANCELED){
+            if(requestCode == WHATSAPP_REQUEST_CODE ){
+                makeToast(getContext(), "failed to share generated link");
+            }
+        }
     }
 
 }
--- app/src/main/java/com/now8/tool/MainActivity.java
+++ app/src/main/java/com/now8/tool/MainActivity.java
@@ -1,5 +1,6 @@
 package com.now8.tool;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeUI();
 
+    }
+
+
+    private void initializeUI(){
+
+        //Log the device year for statistics
         int deviceYearClass = YearClass.get(getApplicationContext());
         Log.d(TAG,"Device year: " + String.valueOf(deviceYearClass) );
 
@@ -29,8 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction.add(R.id.main_activity_frame_layout, generateLinkFragment);
         fragmentTransaction.commit();
-
-
-
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
