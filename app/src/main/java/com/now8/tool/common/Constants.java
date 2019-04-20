1a0ff11e02aa4b9d8107b2cffb07cdec207ee2d5
Ofir Bar <ofirbar5@gmail.com>
2019-04-20 18:38:38 +0300

    removed all location permission requests and requests to turn locations in settings logic from the app in general; and specifically from GenerateRideActivity. removed resources such as strings related to locations as well

--- app/src/main/java/com/now8/tool/common/Constants.java
+++ app/src/main/java/com/now8/tool/common/Constants.java
@@ -8,7 +8,5 @@ public final class Constants {
     public static final int SHARED_IN_SLACK_REQUEST_CODE = 1;
     public static final String AWS_BASE_URL = "https://88xeo0sh9k.execute-api.eu-west-1.amazonaws.com/dev/";
 
-    public static final int REQUEST_TO_TURN_ON_LOCATION_IN_SETTINGS_DIALOG = 61124;
-
 
 }
--- app/src/main/java/com/now8/tool/screens/generate_ride/GenerateRideActivity.java
+++ app/src/main/java/com/now8/tool/screens/generate_ride/GenerateRideActivity.java
@@ -1,14 +1,12 @@
 package com.now8.tool.screens.generate_ride;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
@@ -20,15 +18,6 @@ import com.auth0.android.authentication.AuthenticationException;
 import com.auth0.android.provider.AuthCallback;
 import com.auth0.android.provider.WebAuthProvider;
 import com.auth0.android.result.Credentials;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.tasks.Task;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,20 +25,14 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import com.now8.tool.AbstractUserPermissions;
 import com.now8.tool.FrontendClient;
 import com.now8.tool.R;
 import com.now8.tool.Ride;
 import com.now8.tool.common.Constants;
 
-public class GenerateRideActivity extends AbstractUserPermissions {
+public class GenerateRideActivity extends FragmentActivity {
     private static final String TAG = "GenerateRideActivity";
 
-
-    private static final String STATE_OF_SYSTEM_SETTINGS_DIALOG_KEY ="STATE_OF_SYSTEM_SETTINGS_DIALOG_KEY";
-    private boolean turnOnLocationInSettingsDialogInForeground = false;
-
-    private Location initialLocation;
     private Button createRide;
 
     private Auth0 auth0;
@@ -60,7 +43,6 @@ public class GenerateRideActivity extends AbstractUserPermissions {
         super.onSaveInstanceState(outState);
         Log.d(TAG,"onSaveInstanceState");
 
-        outState.putBoolean(STATE_OF_SYSTEM_SETTINGS_DIALOG_KEY, turnOnLocationInSettingsDialogInForeground);
     }
 
     @Override
@@ -100,152 +82,6 @@ public class GenerateRideActivity extends AbstractUserPermissions {
 
         }
 
-    }
-
-    //TODO : do not work with permissions in any activity. Move them to a "BaseActivity" so in all
-    // activities you will need them you will have them without duplicating code
-    // I saw that you have the "AbstractUserPermissions" but you have to deal with the 'results" too.
-    @Override
-    protected void onRequiredPermissionsGranted(Bundle mainActivityState) {
-        Log.d(TAG,"onRequiredPermissionsGranted");
-
-        if (mainActivityState != null) {
-            turnOnLocationInSettingsDialogInForeground = mainActivityState.getBoolean(STATE_OF_SYSTEM_SETTINGS_DIALOG_KEY);
-        }
-
-
-        if (!turnOnLocationInSettingsDialogInForeground) {
-            turnOnLocationInSettingsDialogInForeground = true;
-
-            LocationSettingsRequest requiredDeviceSettingsForLocationRequests = new LocationSettingsRequest.Builder()
-                    .addLocationRequest(LocationRequest.create())
-                    .build();
-
-            LocationServices.getSettingsClient(this)
-                    .checkLocationSettings(requiredDeviceSettingsForLocationRequests)
-                    .addOnCompleteListener(this::requestLocationViaFusedApi);
-        }
-
-    }
-
-    @Override
-    protected void onUserDeniedRequiredPermissions() {
-        Log.d(TAG,"onUserDeniedRequiredPermissions");
-
-        Toast
-                .makeText(this, R.string.err_no_perm, Toast.LENGTH_LONG)
-                .show();
-        finish();
-    }
-
-    private boolean requestLocationViaFusedApi(Task<LocationSettingsResponse> task) {
-        Log.d(TAG,"requestLocationViaFusedApi");
-
-        try {
-            if(isLocationAvailable(task)){
-                getDeviceLocation();
-            }
-        }
-        catch (ApiException fusedApiException) {
-            turnOnLocationInSettingsDialogOrToastLocationIsNotUsable(fusedApiException);
-            return false;
-        }
-        return false;
-    }
-
-    // checks that GPS or network location provider is usable and present
-    private boolean isLocationAvailable(Task<LocationSettingsResponse> task) throws ApiException{
-        Log.d(TAG,"isLocationAvailable");
-
-        LocationSettingsResponse locationSettingsResponseTaskFromGoogleAPI = task.getResult(ApiException.class);
-        LocationSettingsStates userSettingsLocationState = locationSettingsResponseTaskFromGoogleAPI.getLocationSettingsStates();
-
-        return ( userSettingsLocationState.isLocationPresent() && userSettingsLocationState.isLocationUsable() );
-
-    }
-    private void turnOnLocationInSettingsDialogOrToastLocationIsNotUsable(Exception e) {
-        Log.d(TAG,"turnOnLocationInSettingsDialogOrToastLocationIsNotUsable");
-
-
-        if (e instanceof ResolvableApiException) {
-            try {
-                //Note: startResolutionForResult calls startActivityForResult, and that opens a turnOnLocationInSettingsDialog Activity
-                ((ResolvableApiException)e).startResolutionForResult(this, Constants.REQUEST_TO_TURN_ON_LOCATION_IN_SETTINGS_DIALOG);
-            }
-            // Couldn't pop a turnOnLocationInSettingsDialog
-            catch (IntentSender.SendIntentException intentSenderException) {
-                locationUnavailable(this, intentSenderException);
-            }
-        }
-
-    }
-
-    private void locationUnavailable(Context context, Exception e){
-
-        Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-        Log.e(getClass().getSimpleName(), Integer.toString(R.string.err_failed_to_request_settings_change_dialog), e);
-        finish();
-    }
-
-    @SuppressLint("MissingPermission")
-    private void getDeviceLocation() {
-        Log.d(TAG,"getDeviceLocation");
-
-        FusedLocationProviderClient client=
-                LocationServices.getFusedLocationProviderClient(this);
-
-        // Note: getLastLocation() does no give a Location object, It returns a Task.
-        // The Play Services SDK talks to a separate Play Services Framework app, which in turn handles all of the Play Services work
-        // Using the response from that Play Service we can either get location or fail to.
-
-        client.getLastLocation()
-        .addOnCompleteListener(this, this::locationRequestSuccess);
-                // TODO: add a failure scenario
-    }
-    private void locationRequestSuccess(Task<Location> getUserLocationTask) {
-        Log.d(TAG,"locationRequestSuccess");
-
-        if (getUserLocationTask.getResult() == null) {
-            Toast
-                    .makeText(this, R.string.err_location_null, Toast.LENGTH_LONG)
-                    .show();
-            finish();
-        }
-        else {
-            initialLocation = getUserLocationTask.getResult();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        Log.d(TAG,"onActivityResult");
-
-        if (requestCode == Constants.REQUEST_TO_TURN_ON_LOCATION_IN_SETTINGS_DIALOG) {
-            turnOnLocationInSettingsDialogInForeground = false; // Avoid showing multiply dialogs. (e.g. due to configuration change - screen rotate)
-
-            if (resultCode==RESULT_OK) {
-                getDeviceLocation();
-            }
-            else {
-                Toast.makeText(this, R.string.err_user_denied_system_location_turn_on, Toast.LENGTH_LONG).show();
-                finish();
-            }
-        }
-
-        else if (requestCode == Constants.SHARED_IN_SLACK_REQUEST_CODE){
-
-            if (resultCode == RESULT_OK){
-                Toast.makeText(this, R.string.info_share_ride_success, Toast.LENGTH_LONG).show();
-            }
-            else if (resultCode == RESULT_CANCELED){
-                Toast.makeText(this, R.string.err_share_ride_fail, Toast.LENGTH_LONG).show();
-            }
-        }
-
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
 
     private void shareRideToSlack(String rideUID) {
@@ -294,8 +130,7 @@ public class GenerateRideActivity extends AbstractUserPermissions {
 
             }
 
-
-            //TODO: retrofit calls and everything should be "in one place"
+   //TODO: retrofit calls and everything should be "in one place"
     private void joinRideNetworkRequest(String rideUID){
 
         Retrofit.Builder retrofitConf = new Retrofit.Builder()
@@ -360,7 +195,6 @@ public class GenerateRideActivity extends AbstractUserPermissions {
         else {
             Toast.makeText(this, "User is not logged in", Toast.LENGTH_SHORT).show();
         }
-
 
     }
 
